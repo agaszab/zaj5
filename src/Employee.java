b331c7026d19877029c86988111a984c078dae2c
Agnieszka <gatito@poczta.pl>
2018-01-22 09:18:25 -0800

    zad drugie

---
+++ src/Employee.java
@@ -0,0 +1,40 @@
+public class Employee {
+
+    private String imie;
+    private String nazwisko;
+    private double wyplata;
+
+    public Employee(String imie, String nazwisko, double wyplata) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wyplata=wyplata;
+    }
+
+    public Employee(){
+
+    }
+
+    public double getWyplata() {
+        return wyplata;
+    }
+
+    public void setWyplata(double wyplata) {
+        this.wyplata = wyplata;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+}
